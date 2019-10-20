652a4fa67339b5974ce480b539ad109450a364e2
CaioWzy <CaioWzy@gmail.com>
2019-10-20 16:54:13 -0300

    Adiciona EM

--- src/burlesco.user.js
+++ src/burlesco.user.js
@@ -58,6 +58,7 @@
 // @match        *://*.diarinho.com.br/*
 // @match        *://*.diariodaregiao.com.br/*
 // @match        *://*.correio24horas.com.br/*
+// @match        *://*.em.com.br/*
 // @webRequestItem {"selector":"*://correio-static.cworks.cloud/vendor/bower_components/paywall.js/paywall.js*","action":"cancel"}
 // @webRequestItem {"selector":{"include":"*://paywall.folha.uol.com.br/*","exclude":"*://paywall.folha.uol.com.br/status.php"} ,"action":"cancel"}
 // @webRequestItem {"selector":"*://static.folha.uol.com.br/paywall/*","action":"cancel"}
@@ -379,6 +380,11 @@ document.addEventListener('DOMContentLoaded', function() {
         }
       }
     });
+  }
+
+  else if (/em\.com\.br/.test(document.location.host)) {
+    document.querySelectorAll('.news-blocked').forEach( el => el.remove());
+    document.body.className = document.body.className.replace('news-blocked-no-scroll', '');
   }
 
   if (code !== null) {
