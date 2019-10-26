23aaf9c60d7090a74d6704853d23f87b8460e29b
Caio <CaioWzy@users.noreply.github.com>
2019-10-26 18:03:19 -0300

    Versão 11.17

--- src/burlesco.user.js
+++ src/burlesco.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Burlesco
 // @namespace    https://burles.co/
-// @version      11.15
+// @version      11.17
 // @description  Leia notícias sem ser assinante, burle o paywall
 // @author       rodorgas & AugustoResende
 // @supportURL   https://burles.co
